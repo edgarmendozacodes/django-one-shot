00d0176fac0fa84bfbb2a8c2c3f16dd271747079
Edgar Mendoza <eddiemendoza.02@gmail.com>
2023-12-12 15:59:36 -0600

    Feature 6 Complete

--- todos/models.py
+++ todos/models.py
@@ -9,3 +9,17 @@ class TodoList(models.Model):
     # This tells Django how to convert model into a string 
     def __str__(self):
         return self.name  
+
+class TodoItem(models.Model):
+    task = models.CharField(max_length=100) 
+    due_date = models.DateTimeField(
+        null = True,
+        blank = True,
+    )
+    is_completed = models.BooleanField(default=False)
+    
+    list = models.ForeignKey(
+        TodoList,
+        related_name = "items", 
+        on_delete = models.CASCADE
+    ) 
