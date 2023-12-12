4de65eb14b25d6b7bdb1e9aeabafe235136777fb
Edgar Mendoza <eddiemendoza.02@gmail.com>
2023-12-12 14:31:32 -0600

    Feature 3 Complete

--- todos/models.py
+++ todos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class TodoList(models.Model):
     # This defines the fields of your model # 
     name = models.CharField(max_length = 100) # max length 100 characters
-    created_on = models.DateTimeField(auto_now_add=) # should be automatically set to the time the todolist was created
+    created_on = models.DateTimeField(auto_now_add=True) # should be automatically set to the time the todolist was created
     # This tells Django how to convert model into a string 
     def __str__(self):
         return self.name  
