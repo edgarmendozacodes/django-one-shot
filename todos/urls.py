6adf3d8512a7dd481aef32f6d475d6f9cfd5eff4
Edgar Mendoza <eddiemendoza.02@gmail.com>
2023-12-12 17:26:58 -0600

    Feature 07 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from todos.views import todo_list
 
 urlpatterns = [
-    path("todos/", todo_list, name = "todo_list"),
+    path("", todo_list, name = "todo_list_list"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -6,6 +6,13 @@ from todos.models   import TodoItem
 def todo_list(request):
     todo_list = TodoList.objects.all()
     context = {
-        "todo_list": todo_list
+        "todo_list": todo_list,
     }
-    return render(request, "todos/todos_list.html", context)
+    return render(request, "todos/todos.html", context)
+
+# def show_model_name(request):
+#   model_list = ModelName.objects.all()
+#   context = {
+#     "model_list": model_list
+#   }
+#   return render(request, "model_names/list.html", context)
